adc5b75aa3c305f42a91e1fd9b4c56bed5824180
Anderson Lameck <andylamax@programmer.net>
2019-03-28 17:19:31 +0300

    Added operator functions

--- src/commonMain/kotlin/com/asofttz/money/Money.kt
+++ src/commonMain/kotlin/com/asofttz/money/Money.kt
@@ -1,25 +1,8 @@
 package com.asofttz.money
 
-import kotlinx.serialization.*
-import kotlinx.serialization.internal.StringDescriptor
 import kotlin.math.floor
 
-@Serializable
 data class Money(var value: Double = 0.0, var cur: Currency = Currency.TZS) {
-    @Serializer(forClass = Money::class)
-    companion object : KSerializer<Money> {
-        override val descriptor = StringDescriptor.withName("Money")
-
-        override fun deserialize(input: Decoder): Money {
-            val splits = input.decodeString().split(" ")
-            val cur = Currency.valueOf(splits[0])
-            return Money(splits[1].replace(",", "").toDouble(), cur)
-        }
-
-        override fun serialize(output: Encoder, obj: Money) {
-            output.encodeString(obj.toFullString())
-        }
-    }
 
     operator fun times(n: Number) = Money(n.toDouble() * value, cur)
 
@@ -28,19 +11,31 @@ data class Money(var value: Double = 0.0, var cur: Currency = Currency.TZS) {
         return Money(m.value + value, cur)
     }
 
+    operator fun minus(m: Money): Money {
+        if (m.cur != cur) throw Exception("Can't add between two currencies ${m.cur.name}/${cur.name}")
+        return Money(value - m.value, cur)
+    }
+
     operator fun plusAssign(m: Money) {
         if (m.cur != cur) throw Exception("Can't add between two currencies ${m.cur.name}/${cur.name}")
         value += m.value
+    }
+
+    operator fun compareTo(m: Money): Int {
+        if (m.cur != cur) {
+            throw Exception("Can't compare different currencies")
+        }
+        return value.compareTo(m.value)
     }
 
     fun toFullString(): String = "${cur.name} $this"
 
     override fun toString(): String {
         var out = ""
-        var amount = floor(value * 100) / 100
+        val amount = floor(value * 100) / 100
         val splits = amount.toString().split(".")
-        var characteristic = splits[0]
-        var mantisa = splits.elementAtOrElse(1) { "0" }
+        val characteristic = splits[0]
+        val mantisa = splits.elementAtOrElse(1) { "0" }
         var counts = 0
         for (i in characteristic.length downTo 0) {
             out = characteristic[i] + out
